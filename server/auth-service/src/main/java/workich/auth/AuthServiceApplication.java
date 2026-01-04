fd2af71d2dc0149a30c24a57dd8ba84981a85479
Axeniter <anamievoid@gmail.com>
2026-01-04 19:07:52 +1000

    Create auth service

--- server/auth-service/src/main/java/workich/auth/AuthServiceApplication.java
+++ server/auth-service/src/main/java/workich/auth/AuthServiceApplication.java
@@ -1,13 +1,13 @@
-package com.sp.server;
+package workich.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
