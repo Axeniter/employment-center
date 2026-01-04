4cec704852a09d9e0ca0d917d0fb6b644341ee4b
Axeniter <anamievoid@gmail.com>
2026-01-04 14:48:16 +1000

    Start MAUI project

---
+++ app/EmploymentApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EmploymentApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ app/EmploymentApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EmploymentApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EmploymentApp.Pages")]
