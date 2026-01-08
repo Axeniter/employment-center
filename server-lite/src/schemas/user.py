40d9a7366e7fecf300247538fb172688889ba04a
Axeniter <anamievoid@gmail.com>
2026-01-08 20:51:37 +1000

    Feat(lite): user schemas

---
+++ server-lite/src/schemas/user.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+from model.user import UserRole
+from uuid import UUID
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    role: UserRole
+    password: str
+
+class UserLogin(UserBase):
+    password: str
+    
+class UserResponse(UserBase):
+    id: UUID
+    role: UserRole
+
+    class Config:
+        from_attributes = True
+
