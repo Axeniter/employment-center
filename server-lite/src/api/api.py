c45a1b51a3a780d6e30f72772fd6c15e06bbadb1
Axeniter <anamievoid@gmail.com>
2026-01-19 01:52:13 +1000

    Feat(lite): add vacancy, event and chat endpoints to api router

--- server-lite/src/api/api.py
+++ server-lite/src/api/api.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
-from .endpoints import (auth, profile)
+from .endpoints import (auth, profile, vacancy, event, chat)
 
 api_router = APIRouter(prefix="/api")
 
 api_router.include_router(auth.auth_router)
 api_router.include_router(profile.profile_router)
+api_router.include_router(vacancy.vacancy_router)
+api_router.include_router(vacancy.response_router)
+api_router.include_router(event.event_router)
+api_router.include_router(chat.chat_router)
